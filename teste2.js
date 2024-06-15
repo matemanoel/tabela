5513caf1756772fd4b96383764fc249102909592
matemanoel <142026423+matemanoel@users.noreply.github.com>
2024-06-14 21:16:45 -0300

    Create teste2.js

---
+++ teste2.js
@@ -0,0 +1,8 @@
+document.getElementById('inclusaojs').addEventListener('reset', function() {
+    alert('Formulário limpo!');
+});
+
+document.getElementById('inclusaojs').addEventListener('submit', function() {
+    alert('Formulário enviado com sucesso!');
+});
+
